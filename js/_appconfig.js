a0cf24639e2fbdc179b749fdee089d5d10adf849
rondinele <rondinele_loiola>
2019-09-27 11:50:55 -0300

    270919 agendamento
    foi feito o agendamento do serviço masculino

--- js/_appconfig.js
+++ js/_appconfig.js
@@ -3,13 +3,13 @@
 // Consigurações da conexão com o Google Firebase
 var firebaseConfig = {
     apiKey: "AIzaSyDtNZIxi12reDquEEnyIG2U7sdhF12RZRg",
-        authDomain: "atelieducorpoapp.firebaseapp.com",
-        databaseURL: "https://atelieducorpoapp.firebaseio.com",
-        projectId: "atelieducorpoapp",
-        storageBucket: "atelieducorpoapp.appspot.com",
-        messagingSenderId: "302241854487",
-        appId: "1:302241854487:web:e960970824b52415e15852"
-}
+    authDomain: "atelieducorpoapp.firebaseapp.com",
+    databaseURL: "https://atelieducorpoapp.firebaseio.com",
+    projectId: "atelieducorpoapp",
+    storageBucket: "atelieducorpoapp.appspot.com",
+    messagingSenderId: "302241854487",
+    appId: "1:302241854487:web:e960970824b52415e15852"
+};
 
 // Nome da 'key' que armasena as configurações locais
 var localStorageKeyName = 'config';
@@ -17,4 +17,4 @@ var localStorageKeyName = 'config';
 // Configuração inicial do App
 var initialConfig = {
     tema : 'light' // Tema inicial (light | dark)
-}
+}  
--- js/agendar.js
+++ js/agendar.js
@@ -1,103 +1,19 @@
-<style>
-.pageWrap label {
-    display: block;
-    padding: 0 1rem .2rem .5rem;
-}
-.pageWrap input, .pageWrap textarea, .pageWrap button {
-    font-family: inherit;
-    font-size: inherit;
-    border: 1px solid #ccc;
-    padding: .3rem .5rem;
-    width: 100%;
-    border-radius: .3rem;
-}
-.pageWrap textarea {
-    height: 5rem;
-}
-.pageWrap button {
-    border: 0;
-}
-#msgErroBox, #msgFb {
-    display: none;
-}
-#msgErro {
-    background: #ffff00;
-    color: darkred;
-    font-size: 80%;
-    display: table;
-    margin: auto;
-    margin-top: 1rem;
-    padding: .5rem;
-    border-radius: .3rem;
-}
-#msgErro > span {
-    display: block;
-    float: right;
-    cursor: pointer;
-    border: 1px solid grey;
-    border-radius: .2rem;
-    background: #fff;
-}
-#msgErro > span:hover {
-    background: red;
-    color: #fff;
-}
-#msgErro > h3 {
-    margin: 0;
-}
-</style>
-
-<h2>
-    <a href="#home"><i class="fas fa-fw fa-arrow-left"></i></a>
-    <span>Faça Contato</span>    
-</h2>
-<div class="pageWrap">
-
-<div id="msgFb"></div>
-
-<form action="#contatos" method="post" id="contatos">
-    Preencha o formulário abaixo para entrar em contato com a equipe do 'Atelie du corpo'.
-
-<div id="msgErroBox">
-    <div id="msgErro"></div>
-</div>
-
-<p>
-    <label for="nome">Nome:</label>
-    <input type="text" name="nome" id="nome" placeholder="Seu nome completo.">
-</p>
-<p>
-    <label for="email">E-mail:</label>
-    <input type="text" name="email" id="email" placeholder="Seu e-mail principal.">
-</p>
-<p>
-    <label for="assunto">Assunto:</label>
-    <input type="text" name="assunto" id="assunto" placeholder="Assunto do seu contato.">
-</p>
-<p>
-    <label for="mensagem">Mensagem:</label>
-    <textarea name="mensagem" id="mensagem" placeholder="Sua mensagem."></textarea>
-</p>
-<p>
-    <button class="btnDefault" type="submit" name="enviar" id="enviar">Enviar</button>
-</p>
-</form>
-
-</div>
-
-<script>
 // Oculta barra superior
 $('header').slideUp('fast');
 
 // Pré-preenche nome e e-mail de usuário, se logado
-user = conf.get('user');
-if(user.email != ''){
-    $('#nome').val(user.name);
+if(user !== undefined){
+    $('#nome').val(user.displayName);
     $('#email').val(user.email);
 }
 
+serv = conf.get('servico');
+if(serv !== undefined){
+    $('#servico').val(serv);
+}
+
 // Monitorando envio do formulário
-$(document).on('submit', '#contatos', processaForm);
+$(document).on('submit', '#agendar', processaForm);
 
 // Monitorando botão fechar box
 $(document).on('click', '#msgErro > span', closeBox);
@@ -114,14 +30,16 @@ function processaForm(){
     // Obtendo os valores dos campos já sanitizados
     var formNome = sanitiza($('#nome').val());
     var formEmail = sanitiza($('#email').val());
-    var formAssunto = sanitiza($('#assunto').val());
-    var formMensagem = sanitiza($('#mensagem').val());
+    var formServico = sanitiza($('#servico').val());
+    var formData = sanitiza($('#dataservico').val());
+    var formHorario = sanitiza($('#horarioservico').val());
 
     // Atualizando campos com valores sanitizados
     $('#nome').val(formNome);
     $('#email').val(formEmail);
-    $('#assunto').val(formAssunto);
-    $('#mensagem').val(formMensagem);
+    $('#servico').val(formServico);
+    $('#dataservico').val(formData);
+    $('#horarioservico').val(formHorario);
 
     // Validando nome
     if (formNome.length < 3) {
@@ -138,12 +56,17 @@ function processaForm(){
     }
 
     // Validando assunto
-    if (formAssunto.length < 5) {
-        erro += '<li>O assunto está muito curto.</li>';
+    if (formServico.length < 5) {
+        erro += '<li>O serviço não foi preenchido.</li>';
+    }
+
+    // Validando assunto
+    if (formData.length < 5) {
+        erro += '<li>O serviço não foi preenchido.</li>';
     }
     
     // Validando mensagem
-    if (formMensagem.length < 5) {
+    if (formHorario.length < 5) {
         erro += '<li>A mensagem está muito curta.</li>';
     }
 
@@ -154,12 +77,13 @@ function processaForm(){
         nome = formNome.split(' ');
 
         // Enviando para o Firebase, salvando na coleção 'contatos'
-        db.collection("contatos").add({
+        db.collection("agenda").add({
             data: agoraDb(), // Obtém a data de agora, formatada
             nome: formNome,
             email: formEmail,
-            assunto: formAssunto,
-            mensagem: formMensagem,
+            servico: formServico,
+            data: formData,
+            horario: formHorario,
             status: 'recebido'
         })
         .then(function(docRef) { // Se gravou no Firebase
@@ -169,14 +93,14 @@ function processaForm(){
 Obrigado...
             `;
             $('#msgFb').html(msg);
-            $('#contatos').hide('fast', function(){
+            $('#agendar').hide('fast', function(){
                 $('#msgFb').show('fast');
             });        
         })
         .catch(function(error) { // Se falhou ao gravar no Firebase
             msg = `
 <h3>Ooooops!</h3>
-<p>Ocorreram erros ao tentar enviar o seu contato:</p>
+<p>Ocorreram erros ao agendar o seu servico:</p>
 <ul><li>Falha no servidor remoto.</li>
 <p>A equipe técnica do Atelie du Corpo já foi avisada da falha.</p>
 Você pode tentar enviar o formulário novamente, agora ou mais tarde.
@@ -191,7 +115,7 @@ Você pode tentar enviar o formulário novamente, agora ou mais tarde.
         msg = `
 <span><i class="fas fa-fw fa-times"></i></span>
 <h3>Ooops!</h3>
-<p>Ocorreram erros ao tentar enviar o seu contato:</p>
+<p>Ocorreram erros ao agendar o seu servico:</p>
 <ul>${erro}</ul>
 Por favor, corrija os erros e tente novamente.
 `;
@@ -208,5 +132,3 @@ Por favor, corrija os erros e tente novamente.
 function closeBox(){
     $('#msgErroBox').hide('fast');
 }
-
-</script>
